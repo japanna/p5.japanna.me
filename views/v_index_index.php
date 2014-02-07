d11793fd72952c1b9118d7a1c8c48852ada60294
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-02-07 16:13:27 -0600

    Removed test thumbnails

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -11,30 +11,5 @@
 	          <a href='/topics/playdoh'><img class="thumb" src="http://img.youtube.com/vi/0SDcPW_chIM/0.jpg"></a>
 	          <h2>Playdoh</h2>
 	</div>
-	<div class="home_preview" id="KU-enRMlCp8">
-	          <a href='/topics/lego'><img class="thumb" src="http://img.youtube.com/vi/mkwL2ZzU9lw/0.jpg"></a>
-	          <h2>LEGO</h2>
-	</div>
-	<div class="home_preview" id="KU-enRMlCp8">
-	          <a href='/topics/lego'><img class="thumb" src="http://img.youtube.com/vi/mkwL2ZzU9lw/0.jpg"></a>
-	          <h2>LEGO</h2>
-	</div>
-
-
-	<div class="home_preview" id="KU-enRMlCp8">
-	          <a href='/topics/lego'><img class="thumb" src="http://img.youtube.com/vi/mkwL2ZzU9lw/0.jpg"></a>
-	          <h2>LEGO</h2>
-	</div>
-	<div class="home_preview" id="KU-enRMlCp8">
-	          <a href='/topics/lego'><img class="thumb" src="http://img.youtube.com/vi/mkwL2ZzU9lw/0.jpg"></a>
-	          <h2>LEGO</h2>
-	</div>
-	<div class="home_preview" id="KU-enRMlCp8">
-	          <a href='/topics/lego'><img class="thumb" src="http://img.youtube.com/vi/mkwL2ZzU9lw/0.jpg"></a>
-	          <h2>LEGO</h2>
-	</div>
-	<div class="home_preview" id="KU-enRMlCp8">
-	          <a href='/topics/lego'><img class="thumb" src="http://img.youtube.com/vi/mkwL2ZzU9lw/0.jpg"></a>
-	          <h2>LEGO</h2>
-	</div>
+	
 </div>
